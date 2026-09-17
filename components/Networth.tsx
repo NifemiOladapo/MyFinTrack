c38bb61d11e388c72d9501deaa5d7fcd3e5f7959
Nifemi Oladapo <nifethedev@gmail.com>
2026-09-17 17:44:42 +0100

    changed text style to match others

--- components/Networth.tsx
+++ components/Networth.tsx
@@ -1,7 +1,11 @@
 const Networth = async () => {
   await new Promise((res) => setTimeout(res, 3000));
 
-  return <div className="flex-1 p-5">Net worth, last 30 days</div>;
+  return (
+    <div className="flex-1 p-5">
+      <h2 className="mb-6 text-sm font-medium text-neutral-300">Net worth, last 30 days</h2>
+    </div>
+  );
 };
 
 export default Networth;
